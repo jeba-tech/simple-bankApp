91a2dbafb719c5f54bc9b1194c78bf2f8698bf20
JEBA <fawjiajeba@gmail.com>
2022-02-11 19:48:35 +0600

    deposite section

--- js/banking.js
+++ js/banking.js
@@ -1,14 +1,30 @@
 //handle deposite button event
 document.getElementById('deposite-button').addEventListener('click', function () {
       //get the amount deposited
-      document.getElementById('deposite-input').value;
-      console.log(document.getElementById('deposite-input').value);
-      document.getElementById('deposite-total').innerText = document.getElementById('deposite-input').value;
+      const depositeInput = document.getElementById('deposite-input');
+      const depositeInputValue = depositeInput.value;
+      const parseDepositeInputValue = parseInt(depositeInputValue);
+
+      const depositeTotal = document.getElementById('deposite-total');
+      const depositeTotalValue = depositeTotal.innerText;
+      const parseDepositeTotalValue = parseInt(depositeTotalValue);
+      //updated deposite amount
+      const newDeposite = parseDepositeTotalValue + parseDepositeInputValue;
+      depositeTotal.innerText = newDeposite;
+
+      // updated total balance amount
+      const balanceTotal = document.getElementById('balance-total');
+      const balanceToatalvalue = balanceTotal.innerText;
+      const parseBalanceToatalvalue = parseInt(balanceToatalvalue);
+
+      const newBalance = parseBalanceToatalvalue + parseDepositeInputValue;
+      balanceTotal.innerText = newBalance;
+
       //clear the deposite input field
       document.getElementById('deposite-input').value = '';
 
-})
-//handle withdraw button event
+});
+//handle withdraw event handler
 
 document.getElementById('withdraw-button').addEventListener('click', function () {
       console.log('button cloked')
