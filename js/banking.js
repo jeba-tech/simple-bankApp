69c229b7f1b79a4a58d1a77ec6c1e1290d0eef94
JEBA <fawjiajeba@gmail.com>
2022-02-12 16:14:23 +0600

    final version

--- js/banking.js
+++ js/banking.js
@@ -1,51 +1,93 @@
 //handle deposite button event
-document.getElementById('deposite-button').addEventListener('click', function () {
-      //get the amount deposited
-      const depositeInput = document.getElementById('deposite-input');
-      const depositeInputValue = depositeInput.value;
-      const parseDepositeInputValue = parseFloat(depositeInputValue);
 
-      const depositeTotal = document.getElementById('deposite-total');
-      const depositeTotalValue = depositeTotal.innerText;
-      const parseDepositeTotalValue = parseFloat(depositeTotalValue);
-      //updated deposite amount
-      const newDeposite = parseDepositeTotalValue + parseDepositeInputValue;
-      depositeTotal.innerText = newDeposite;
+function getInputValue(inputId) {
+      //get the amount deposited and withdrawn
 
+      const depositeWithdrawInput = document.getElementById(inputId);
+      const depositeWithdrawInputValue = depositeWithdrawInput.value;
+      const parseDepositeWithdrawInputValue = parseFloat(depositeWithdrawInputValue);
+
+      //clear the deposite input field
+      document.getElementById(inputId).value = '';
+      return parseDepositeWithdrawInputValue;
+}
+
+function updateAmount(updateTotal, inputAmount) {
+      //updated deposite and withdraw amount
+      const depositeWithdrawTotal = document.getElementById(updateTotal);
+      const depositeWithdrawTotalValue = depositeWithdrawTotal.innerText;
+      const parseDepositeWithdrawTotalValue = parseFloat(depositeWithdrawTotalValue);
+      depositeWithdrawTotal.innerText = parseDepositeWithdrawTotalValue + inputAmount;
+}
+
+function updateBalance(updateBalance, isAdd) {
       // updated total balance amount
       const balanceTotal = document.getElementById('balance-total');
       const balanceToatalvalue = balanceTotal.innerText;
       const parseBalanceToatalvalue = parseFloat(balanceToatalvalue);
 
-      const newBalanceDeposite = parseBalanceToatalvalue + parseDepositeInputValue;
-      balanceTotal.innerText = newBalanceDeposite;
+      if (isAdd == true) {
+            balanceTotal.innerText = parseBalanceToatalvalue + updateBalance;
 
-      //clear the deposite input field
-      document.getElementById('deposite-input').value = '';
+      }
+      else {
+            balanceTotal.innerText = parseBalanceToatalvalue - updateBalance;
 
+      }
+
+}
+document.getElementById('deposite-button').addEventListener('click', function () {
+
+      const parseDepositeInputValue = getInputValue('deposite-input');
+      updateAmount('deposite-total', parseDepositeInputValue);
+      updateBalance(parseDepositeInputValue, true);
+
+
+      //another way without uning function
+
+      /* //get the amount deposited
+       const depositeInput = document.getElementById('deposite-input');
+       const depositeInputValue = depositeInput.value;
+       const parseDepositeInputValue = parseFloat(depositeInputValue);
+       const depositeTotal = document.getElementById('deposite-total');
+       const depositeTotalValue = depositeTotal.innerText;
+       const parseDepositeTotalValue = parseFloat(depositeTotalValue);
+       //updated deposite amount
+       const newDeposite = parseDepositeTotalValue + parseDepositeInputValue;
+       depositeTotal.innerText = newDeposite;
+       // updated total balance amount
+       const balanceTotal = document.getElementById('balance-total');
+       const balanceToatalvalue = balanceTotal.innerText;
+       const parseBalanceToatalvalue = parseFloat(balanceToatalvalue);
+
+       const newBalanceDeposite = parseBalanceToatalvalue + parseDepositeInputValue;
+       balanceTotal.innerText = newBalanceDeposite;
+ */
 });
+
 //handle withdraw event handler
 
 document.getElementById('withdraw-button').addEventListener('click', function () {
-      const withdrawInput = document.getElementById('withdraw-input');
-      const withdrawInputValue = withdrawInput.value;
-      const parseWithdrawInputValue = parseFloat(withdrawInputValue);
 
-      const withdrawTotal = document.getElementById('withdraw-total');
-      const withdrawTotalValue = withdrawTotal.innerText;
-      const parseWithdrawTotalValue = parseFloat(withdrawTotalValue);
+      const parseWithdrawInputValue = getInputValue('withdraw-input');
+      updateAmount('withdraw-total', parseWithdrawInputValue);
+      updateBalance(parseWithdrawInputValue, false);
 
-      //update withdraw 
-      const newWithdraw = parseWithdrawInputValue + parseWithdrawTotalValue;
-      withdrawTotal.innerText = newWithdraw;
+      //another way without uning function
+      /*    const withdrawInput = document.getElementById('withdraw-input');
+         const withdrawInputValue = withdrawInput.value;
+         const parseWithdrawInputValue = parseFloat(withdrawInputValue);
+        const withdrawTotal = document.getElementById('withdraw-total');
+         const withdrawTotalValue = withdrawTotal.innerText;
+         const parseWithdrawTotalValue = parseFloat(withdrawTotalValue);
+         //update withdraw 
+         const newWithdraw = parseWithdrawInputValue + parseWithdrawTotalValue;
+         withdrawTotal.innerText = newWithdraw;
+        update balance based on the withdraw 
+         const balanceTotal = document.getElementById('balance-total');
+         const balanceToatalvalue = balanceTotal.innerText;
+         const parseBalanceToatalvalue = parseFloat(balanceToatalvalue);
+         const newBalanceWithdraw = parseBalanceToatalvalue - parseWithdrawInputValue;
+         balanceTotal.innerText = newBalanceWithdraw; */
 
-      //update balance based on the withdraw 
-      const balanceTotal = document.getElementById('balance-total');
-      const balanceToatalvalue = balanceTotal.innerText;
-      const parseBalanceToatalvalue = parseFloat(balanceToatalvalue);
-
-      const newBalanceWithdraw = parseBalanceToatalvalue - parseWithdrawInputValue;
-      balanceTotal.innerText = newBalanceWithdraw;
-      //clear the withdraw input field
-      document.getElementById('withdraw-input').value = '';
-})
+});
